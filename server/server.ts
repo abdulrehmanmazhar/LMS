4139c5c8f6564b68106d15ff6522715eb3249f18
Abdur Rahman <abdulrehmanmazhar02@gmail.com>
2024-11-14 23:52:12 +0500

    mongoDb connected and redis's been set up

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,9 @@
 import { app } from "./app";
 require("dotenv").config();
+import connectDB from "./utils/db";
 
 // create server
 app.listen(process.env.PORT, ()=>{
     console.log("Server is running on", process.env.PORT);
+    connectDB();
 })
