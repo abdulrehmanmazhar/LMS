41f743388afc01c4147e43a5071c62e6b093adfc
Abdur Rahman <abdulrehmanmazhar02@gmail.com>
2024-11-19 00:16:16 +0500

    done answering question and pushing email notification to one who questioned

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -6,7 +6,9 @@ import { createCourse } from "../services/course.service";
 import CourseModel from "../models/course.model";
 import { redis } from "../utils/redis";
 import mongoose from "mongoose";
-
+import ejs from "ejs"
+import path from "path";
+import sendMail from "../utils/sendMail";
 
 export const uploadCourse = CatchAsyncError(async(req: Request, res: Response, next: NextFunction)=>{
     try {
@@ -173,6 +175,77 @@ export const addQuestion = CatchAsyncError(async(req: Request, res: Response, ne
         }
         courseContent.question.push(newQuestion);
         await course?.save();
+        res.status(200).json({
+            success: true,
+            course
+        })
+    } catch (error) {
+        return next(new ErrorHandler(error.message, 500))
+        
+    }
+})
+
+// add answers to questions 
+
+interface IAnswerData{
+    answer : string;
+    courseId : string;
+    contentId: string;
+    questionId : string;
+
+}
+
+export const addAnswer = CatchAsyncError(async(req: Request, res: Response, next: NextFunction)=>{
+    try {
+        const {answer, courseId, contentId, questionId} : IAnswerData = req.body
+        const course = await CourseModel.findById(courseId)
+
+        if(!mongoose.Types.ObjectId.isValid(contentId)){
+            return next(new ErrorHandler("Invalid content Id",400))
+        }
+
+        const courseContent = course?.courseData?.find((item: any)=>{
+            return item._id.equals(contentId)
+        })
+
+        if(!courseContent){
+            return next(new ErrorHandler("Invalid content Id",400))
+
+        }
+
+        const question = courseContent?.question?.find((item:any)=>
+            item._id.equals(questionId)
+        )
+        if(!question){
+            return next(new ErrorHandler("Invalid question Id",400))
+        }
+        const newAnswer : any = {
+            user: req.user,
+            answer
+        }
+        question.questionReplies.push(newAnswer);
+        await course?.save();
+
+        if(req.user?._id === question.user._id){
+            // create a notification
+        }else{
+            const data = {
+                name: question.user.name,
+                title: courseContent.title,
+            }
+            const html = await ejs.renderFile(path.join(__dirname, "../mails/question-reply.ejs"), data)
+
+            try {
+                await sendMail({
+                    email: question.user.email,
+                    subject: "Question Reply",
+                    template: "question-reply.ejs",
+                    data
+                })
+            } catch (error) {
+                return next(new ErrorHandler(error.message, 500))
+            }
+        }
         res.status(200).json({
             success: true,
             course
--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,6 +1,6 @@
 import express  from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
-import { addQuestion, editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller";
+import { addAnswer, addQuestion, editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller";
 const router = express.Router();
 
 router.post("/create-course", isAuthenticated, authorizeRoles("admin"), uploadCourse );
@@ -10,5 +10,6 @@ router.get("/get-all-courses", getAllCourses );
 router.get("/get-course-content/:id",isAuthenticated, getCourseByUser );
 router.get("/get-course-content/:id",isAuthenticated, getCourseByUser );
 router.put("/add-question",isAuthenticated, addQuestion );
+router.put("/add-answer",isAuthenticated, addAnswer );
 
 export default router;
